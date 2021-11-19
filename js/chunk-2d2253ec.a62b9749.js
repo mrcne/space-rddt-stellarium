d17f10ad99c3f103eadbeea69499a1828ba49dc7
mrcne <mrcne@users.noreply.github.com>
2021-11-19 15:32:48 +0000

    deploy: b229ea30a1ac466c6c209bfa42b660b3413e598f

---
+++ js/chunk-2d2253ec.a62b9749.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp"]=this["webpackJsonp"]||[]).push([["chunk-2d2253ec"],{e429:function(e,s,n){e.exports=n.p+"js/stellarium-web-engine.e61abd2d.wasm"}}]);
+//# sourceMappingURL=chunk-2d2253ec.a62b9749.js.map
